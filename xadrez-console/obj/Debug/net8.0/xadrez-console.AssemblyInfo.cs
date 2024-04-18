0a7c36ef4f9ee1bf678171db4010feb970da794e
MarcelaEduarda <marcela.valentim@outlook.com>
2024-04-18 18:59:58 -0300

    Melhorando a implementação do metodo imprimirTabuleiro

--- xadrez-console/obj/Debug/net8.0/xadrez-console.AssemblyInfo.cs
+++ xadrez-console/obj/Debug/net8.0/xadrez-console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb487c0950761c147594df5532a42fbfb9727c95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a034edaf577bb8c43237146d92f5d7c1cafc751e")]
 [assembly: System.Reflection.AssemblyProductAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
