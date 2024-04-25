9ec08169e96b775ffcb4cfcdb2566b197a2f9757
MarcelaEduarda <marcela.valentim@outlook.com>
2024-04-25 17:17:57 -0300

    Jogada especial promocao

--- xadrez-console/obj/Debug/net8.0/xadrez-console.AssemblyInfo.cs
+++ xadrez-console/obj/Debug/net8.0/xadrez-console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8af91cc31480da5ed93bc415f5fcdcaee4a1539")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddd6f5a060177ecdc3c693f4d75be0889e477856")]
 [assembly: System.Reflection.AssemblyProductAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("xadrez-console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
